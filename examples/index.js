7b4c2dbc71b07d8b651667a9a40df977e1cdbd1c
soufyakoub <splentercell.1997@gmail.com>
2020-12-07 19:57:58 +0100

    Setup development environment

---
+++ examples/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Grid from "@material-ui/core/Grid";
+import PhoneInput from "../dist/bundle.js";
+
+const input_props = [
+	{ size: "medium", variant: "outlined" },
+	{ size: "medium", variant: "filled" },
+	{ size: "medium", variant: "standard" },
+	{ size: "small", variant: "outlined" },
+	{ size: "small", variant: "filled" },
+	{ size: "small", variant: "standard" }
+];
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Grid container spacing={2}>
+			{input_props.map(props => <Grid item key={props.size + props.variant} >
+				<PhoneInput {...props} label="Phone number" />
+			</Grid>)}
+		</Grid>
+	</React.StrictMode>,
+	document.getElementById('root')
+);
