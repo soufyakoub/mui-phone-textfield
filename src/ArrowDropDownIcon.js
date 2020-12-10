7e74ffa9338b6b0e42f9ba7a185cc15581ef8a38
soufyakoub <splentercell.1997@gmail.com>
2020-12-10 17:00:05 +0100

    Add the countries menu

---
+++ src/ArrowDropDownIcon.js
@@ -0,0 +1,8 @@
+import React from "react";
+import SvgIcon from "@material-ui/core/SvgIcon";
+
+export default function ArrowDropDownIcon(props) {
+	return <SvgIcon {...props}>
+		<path d="M7 10l5 5 5-5z" />
+	</SvgIcon>;
+}
