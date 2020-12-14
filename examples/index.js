734611eaa602521a39b6200f784fa98c17b2731b
soufyakoub <splentercell.1997@gmail.com>
2020-12-14 18:58:22 +0100

    Process the input value

--- examples/index.js
+++ examples/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Grid from "@material-ui/core/Grid";
 import PhoneInput from "..";
@@ -12,12 +12,26 @@ const input_props = [
 	{ size: "small", variant: "standard" }
 ];
 
+function Example(props) {
+	const [phoneNumber, setPhoneNumber] = useState();
+
+	return <>
+		<PhoneInput {...props} label="Phone number" error={!phoneNumber} onChange={setPhoneNumber} />
+		<br /><br />
+		<div>International: {phoneNumber?.format("INTERNATIONAL")}</div>
+		<div>National: {phoneNumber?.format("NATIONAL")}</div>
+		<div>E.164: {phoneNumber?.format("E.164")}</div>
+		<div>Country: {phoneNumber?.country}</div>
+	</>;
+}
+
 ReactDOM.render(
 	<React.StrictMode>
 		<Grid container spacing={2}>
-			{input_props.map(props => <Grid item key={props.size + props.variant} >
-				<PhoneInput {...props} label="Phone number" />
-			</Grid>)}
+			{input_props.map(props =>
+				<Grid item key={props.size + props.variant} >
+					<Example {...props} />
+				</Grid>)}
 		</Grid>
 	</React.StrictMode>,
 	document.getElementById('root')
