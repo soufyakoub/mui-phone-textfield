de340684cb2f467de87c8b84763accc7ef806a3f
soufyakoub <splentercell.1997@gmail.com>
2020-12-23 22:12:40 +0100

    style: convert indentation to tabs

--- sprite.py
+++ sprite.py
@@ -18,8 +18,8 @@ num_row = int(sqrt(len(flag_paths)))
 num_col = ceil(len(flag_paths) / num_row)
 
 sprite_size = (
-    width_col * num_col + padding * (num_col - 1),
-    height_row * num_row + padding * (num_row - 1),
+	width_col * num_col + padding * (num_col - 1),
+	height_row * num_row + padding * (num_row - 1),
 )
 
 # We'll collect flag positions in the sprite image so we can generate the corresponding CSS classes later.
@@ -39,30 +39,30 @@ sprite.paste(Image.open("flags/default.png"), (0, 0))
 
 # Filling columns from top to bottom, and from left to right.
 for index, path in enumerate(flag_paths, 1):
-    x = (width_col + padding) * (index // num_row)
-    y = (height_row + padding) * (index % num_row)
-    country_code = os.path.basename(path).split(".")[0]
+	x = (width_col + padding) * (index // num_row)
+	y = (height_row + padding) * (index % num_row)
+	country_code = os.path.basename(path).split(".")[0]
 
-    flag = Image.open(path)
-    sprite.paste(flag, (x, y))
-    flag.close()
-    flag_positions.append({"country_code": country_code, "x": x, "y": y})
+	flag = Image.open(path)
+	sprite.paste(flag, (x, y))
+	flag.close()
+	flag_positions.append({"country_code": country_code, "x": x, "y": y})
 
 # Generating the sprite's corresponding css classes.
 loader = jinja2.FileSystemLoader("./")
 env = jinja2.Environment(loader=loader)
 template = env.get_template("src/sprite.css.jinja")
 sprite_css = template.render(
-    {
-        "path": "sprite.png",  # The sprite's relative path from the src directory.
-        "width": sprite_size[0],
-        "height": sprite_size[1],
-        "flag": {"width": width_col, "height": height_row, "positions": flag_positions},
-    }
+	{
+		"path": "sprite.png",  # The sprite's relative path from the src directory.
+		"width": sprite_size[0],
+		"height": sprite_size[1],
+		"flag": {"width": width_col, "height": height_row, "positions": flag_positions},
+	}
 )
 
 with open("src/sprite.css", "w") as file:
-    file.write(sprite_css)
+	file.write(sprite_css)
 
 print("Generated sprite.css")
 
