0cef17edd8665040cb79481ffa5543dc83a0ce2c
soufyakoub <splentercell.1997@gmail.com>
2020-12-23 12:41:26 +0100

    build(examples): generate index.html from template

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,13 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = env => ({
 	mode: env.NODE_ENV === "production" ? "production" : "development",
 	entry: path.resolve(__dirname, "index.js"),
 	output: {
 		path: path.resolve(__dirname, 'public'),
-		filename: 'bundle.js',
+		filename: '[name].[chunkhash].js',
+		publicPath: env.NODE_ENV === "production"
+			// This is the base path in the github-pages examples website. (the name of the github repository)
+			? "/mui-phone-textfield"
+			: "/",
 	},
-
 	module: {
 		rules: [
 			{
@@ -19,6 +23,11 @@ module.exports = env => ({
 			},
 		],
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "index.html"),
+		})
+	],
 	devServer: {
 		contentBase: path.join(__dirname, 'public'),
 		port: 3000,
