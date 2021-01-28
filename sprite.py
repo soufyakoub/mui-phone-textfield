fe3c29874d744e7fc525b6a43334d2dece4f21c3
soufyakoub <splentercell.1997@gmail.com>
2021-01-28 20:36:27 +0100

    refactor(sprite): separate python logic from jinja template

--- sprite.py
+++ sprite.py
@@ -13,60 +13,72 @@ padding = 4
 
 # Here, we assume that all images have the same size
 # and we calculate the dimensions of the generated sprite.
-width_col, height_row = Image.open(flag_paths[0]).size
-num_row = int(sqrt(len(flag_paths)))
-num_col = ceil(len(flag_paths) / num_row)
-
-sprite_size = (
-	width_col * num_col + padding * (num_col - 1),
-	height_row * num_row + padding * (num_row - 1),
-)
-
-# We'll collect flag positions in the sprite image so we can generate the corresponding CSS classes later.
-flag_positions = []
+flag_width, flag_height = Image.open(flag_paths[0]).size
+num_rows = int(sqrt(len(flag_paths)))
+num_cols = ceil(len(flag_paths) / num_rows)
 
 # The full sprite image object.
-sprite = Image.new("RGBA", sprite_size)
+sprite = Image.new(
+    "RGBA",
+    (
+        flag_width * num_cols + padding * (num_cols - 1),
+        flag_height * num_rows + padding * (num_rows - 1),
+    ),
+)
 
 # This default flag will be used if a certain country code does not have a corresponding flag in the sprite.
 try:
-	# On Linux
-	flag_paths.remove("flags/default.png")
+    # On Linux
+    flag_paths.remove("flags/default.png")
 except ValueError:
-	# On Windows
-	flag_paths.remove("flags\\default.png")
+    # On Windows
+    flag_paths.remove("flags\\default.png")
 sprite.paste(Image.open("flags/default.png"), (0, 0))
+
+# We'll collect flag positions in the sprite image so we can generate the corresponding CSS classes later.
+flag_positions = {}
 
 # Filling columns from top to bottom, and from left to right.
 for index, path in enumerate(flag_paths, 1):
-	x = (width_col + padding) * (index // num_row)
-	y = (height_row + padding) * (index % num_row)
-	country_code = os.path.basename(path).split(".")[0]
+    x = (flag_width + padding) * (index // num_rows)
+    y = (flag_height + padding) * (index % num_rows)
+    country_code = os.path.basename(path).split(".")[0]
 
-	flag = Image.open(path)
-	sprite.paste(flag, (x, y))
-	flag.close()
-	flag_positions.append({"country_code": country_code, "x": x, "y": y})
+    flag = Image.open(path)
+    sprite.paste(flag, (x, y))
+    flag.close()
+    flag_positions[country_code] = {"x": x, "y": y}
 
 # Generating the sprite's corresponding css classes.
 loader = jinja2.FileSystemLoader("./")
 env = jinja2.Environment(loader=loader)
 template = env.get_template("src/sprite.css.jinja")
-sprite_css = template.render(
-	{
-		"path": "sprite.png",  # The sprite's relative path from the src directory.
-		"width": sprite_size[0],
-		"height": sprite_size[1],
-		"flag": {"width": width_col, "height": height_row, "positions": flag_positions},
-	}
+
+# We will pass half the values to the css template,
+# because to support retina displays,
+# the sprite actually has double the size.
+css = template.render(
+    {
+        "sprite": {
+            "path": "sprite.png",  # The sprite's relative path from the src directory.
+            "width": sprite.size[0] // 2,
+            "height": sprite.size[1] // 2,
+        },
+        "flag": {
+            "width": flag_width // 2,
+            "height": flag_height // 2,
+            "positions": {
+                country_code: {"x": position["x"] // 2, "y": position["y"] // 2}
+                for country_code, position in flag_positions.items()
+            },
+        },
+    }
 )
 
 with open("src/sprite.css", "w") as file:
-	file.write(sprite_css)
-
+    file.write(css)
 print("Generated sprite.css")
 
 # Quantize the result sprite to reduce its size then save it.
 sprite.quantize().save("src/sprite.png")
-
 print("Generated sprite.png")
