fc334890903a74d5037fe8aa30e5a82287d37143
soufyakoub <splentercell.1997@gmail.com>
2021-01-29 19:09:22 +0100

    refactor: move CountriesMenu to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
-import React, { ChangeEvent } from "react";
-import PropTypes from 'prop-types';
+import React, { ChangeEvent, useState, MouseEvent, useRef, KeyboardEvent, useEffect, forwardRef } from "react";
+import PropTypes from "prop-types";
 import TextField, { TextFieldProps } from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
-import CountriesMenu from "./CountriesMenu";
-import { CountryCode, AsYouType, PhoneNumber, getCountries } from "libphonenumber-js";
+import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Popover from "@material-ui/core/Popover";
+import { makeStyles } from "@material-ui/core/styles";
+import { AsYouType, PhoneNumber, getCountries, getCountryCallingCode, CountryCode } from "libphonenumber-js";
+import { FixedSizeList, ListChildComponentProps } from "react-window";
+import ArrowDropDownIcon from "./ArrowDropDownIcon";
+import Flag from "./Flag";
+import territoriesJson from "cldr-localenames-full/main/en/territories.json";
 
 type OnCountrySelectData = {
 	/** The formatted value for the selected country. Extracted from the `value` prop. */
@@ -40,6 +50,24 @@ export type PhoneTextFieldProps = Omit<TextFieldProps, "onChange" | "select" | "
 	onChange?: (data: OnChangeData) => void,
 };
 
+const useStyles = makeStyles(() => ({
+	button: {
+		padding: 2,
+	},
+	flag: {
+		borderRadius: 4,
+	},
+}));
+
+const defaultTerritoryDisplayNames = territoriesJson.main.en.localeDisplayNames.territories;
+const menuData = getCountries()
+	.map(countryCode => ({
+		countryCode,
+		callingCode: getCountryCallingCode(countryCode),
+		countryName: defaultTerritoryDisplayNames[countryCode],
+	}))
+	.sort((a, b) => a.countryName.localeCompare(b.countryName));
+
 const PhoneTextField = (props: PhoneTextFieldProps) => {
 	const {
 		onCountrySelect,
@@ -51,8 +79,59 @@ const PhoneTextField = (props: PhoneTextFieldProps) => {
 		...rest
 	} = props;
 
-	const handleMenuItemClick = ({ countryCode }: { countryCode: CountryCode }) => {
+	const classes = useStyles();
+	const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+	const [currentIndex, setCurrentIndex] = useState(0);
+	const [itemsFocusable, setItemsFocusable] = useState(false);
+	const fixedSizeListRef = useRef<FixedSizeList>(null);
+	const open = Boolean(anchorEl);
+	const ITEM_SIZE = 60;
+	const MENU_WIDTH = 300;
+	// specZ: The maximum height of a simple menu should be one or more rows less than the view
+	// height. This ensures a tapable area outside of the simple menu with which to dismiss
+	// the menu.
+	const MENU_HEIGHT = window.innerHeight - ITEM_SIZE * 2;
+
+	const internalOnChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+		if (!onChange) return;
+
+		const formatter = new AsYouType(country);
+		const formattedValue = formatter.input(event.target.value);
+		const phoneNumber = formatter.getNumber();
+
+		const data = {
+			formattedValue,
+			phoneNumber: phoneNumber?.isValid() ? phoneNumber : undefined,
+			event,
+		};
+
+		onChange(data);
+	};
+
+	const handleButtonClick = (event: MouseEvent<HTMLElement>) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	const handleEntered = () => {
+		setItemsFocusable(true);
+	};
+
+	const handleExited = () => {
+		setCurrentIndex(0);
+		setItemsFocusable(false);
+	};
+
+	const handleMenuItemClick = (event: MouseEvent<HTMLElement>) => {
+		handleClose();
+
 		if (!onCountrySelect) return;
+
+		const dataset = event.currentTarget.dataset;
+		const countryCode = dataset.countryCode as CountryCode;
 
 		const data: OnCountrySelectData = {
 			country: countryCode,
@@ -73,40 +152,172 @@ const PhoneTextField = (props: PhoneTextFieldProps) => {
 		onCountrySelect(data);
 	};
 
-	const internalOnChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-		if (!onChange) return;
-
-		const formatter = new AsYouType(country);
-		const formattedValue = formatter.input(event.target.value);
-		const phoneNumber = formatter.getNumber();
-
-		const data = {
-			formattedValue,
-			phoneNumber: phoneNumber?.isValid() ? phoneNumber : undefined,
-			event,
-		};
-
-		onChange(data);
+	const focusItemByIndex = (index: number) => {
+		setCurrentIndex(index);
+		fixedSizeListRef.current?.scrollToItem(index, "auto");
 	};
 
-	return <TextField
-		{...rest}
-		select={false}
-		type="tel"
-		value={value}
-		onChange={onChange ? internalOnChange : undefined}
-		InputProps={{
-			...InputProps,
-			startAdornment: <InputAdornment position="start">
-				<CountriesMenu
-					selectedCountry={country}
-					countryDisplayNames={countryDisplayNames}
-					onItemClick={handleMenuItemClick}
-				/>
-			</InputAdornment>,
-		}}
-	/>;
+	const handleMenuKeyDown = (event: KeyboardEvent) => {
+		event.preventDefault();
+
+		switch (event.key) {
+
+			case "Tab":
+			case "Escape":
+				handleClose();
+				break;
+
+			case "ArrowUp":
+				let previous_index = currentIndex - 1;
+
+				if (previous_index < 0) {
+					previous_index = menuData.length - 1;
+				}
+
+				focusItemByIndex(previous_index);
+				break;
+
+			case "ArrowDown":
+				let next_index = currentIndex + 1;
+
+				if (next_index >= menuData.length) {
+					next_index = 0;
+				}
+
+				focusItemByIndex(next_index);
+				break;
+
+			case "End":
+				focusItemByIndex(menuData.length - 1);
+				break;
+
+			case "Home":
+				focusItemByIndex(0);
+				break;
+
+			default: break;
+		}
+	}
+
+	return <>
+
+		<TextField
+			{...rest}
+			select={false}
+			type="tel"
+			value={value}
+			onChange={onChange ? internalOnChange : undefined}
+			InputProps={{
+				...InputProps,
+				startAdornment: <InputAdornment position="start">
+					<Button
+						aria-haspopup="menu"
+						aria-expanded={open}
+						aria-controls="countries"
+						onClick={handleButtonClick}
+						className={classes.button}
+					>
+						<Flag countryCode={country} className={classes.flag} />
+						<ArrowDropDownIcon />
+					</Button>
+				</InputAdornment>,
+			}}
+		/>
+
+		<Popover
+			anchorEl={anchorEl}
+			onEntered={handleEntered}
+			open={open}
+			onExited={handleExited}
+			onClose={handleClose}
+			onKeyDown={handleMenuKeyDown}
+		>
+			<FixedSizeList
+				itemData={{
+					handleMenuItemClick,
+					countryDisplayNames,
+					currentIndex,
+					itemsFocusable,
+				}}
+				ref={fixedSizeListRef}
+				innerElementType={FixedSizeListInnerElement}
+				height={MENU_HEIGHT}
+				width={MENU_WIDTH}
+				itemSize={ITEM_SIZE}
+				itemCount={menuData.length}
+			>
+				{FixedSizeListItem}
+
+			</FixedSizeList>
+		</Popover>
+	</>
 }
+
+function FixedSizeListItem({ index, style, data }: ListChildComponentProps) {
+	const {
+		handleMenuItemClick,
+		countryDisplayNames,
+		currentIndex,
+		itemsFocusable,
+	} = data;
+
+	const {
+		countryCode,
+		countryName,
+		callingCode,
+	} = menuData[index];
+
+	const countryDisplayName = countryDisplayNames?.[countryCode] || countryName;
+	const should_be_focused = currentIndex === index;
+
+	const ref = useRef<HTMLLIElement>(null);
+	const classes = useStyles();
+
+	useEffect(() => {
+		if (should_be_focused && itemsFocusable) {
+			ref.current?.focus();
+		}
+	}, [should_be_focused, itemsFocusable]);
+
+	return <ListItem
+		style={style}
+		ref={ref}
+		// Somehow the orca screen reader isn't working unless I use a roving tabindex.
+		tabIndex={should_be_focused ? 0 : -1}
+		component="li"
+		role="menuitem"
+		onClick={handleMenuItemClick}
+		data-country-code={countryCode}
+		data-calling-code={callingCode}
+		button
+		dense
+	>
+		<ListItemIcon>
+			<Flag countryCode={countryCode} className={classes.flag} />
+		</ListItemIcon>
+		<ListItemText
+			primaryTypographyProps={{
+				"aria-label": `country name is ${countryDisplayName}`,
+			}}
+			secondaryTypographyProps={{
+				"aria-label": `calling code is ${callingCode}`,
+			}}
+			primary={countryDisplayName}
+			secondary={"+" + callingCode}
+		/>
+	</ListItem>;
+}
+
+const FixedSizeListInnerElement = forwardRef<HTMLOListElement, any>((props, ref) => {
+	return <List
+		{...props}
+		component="ol"
+		role="menu"
+		aria-label="countries"
+		ref={ref}
+		disablePadding
+	/>
+});
 
 const countryCodes = getCountries();
 const countryDisplayNamesShape = countryCodes.reduce(
